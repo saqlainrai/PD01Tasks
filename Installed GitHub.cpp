c6027d40c01b29cbb40004119c93c3f716534155
Saqlain Mansab <saqlainmansab786@gmail.com>
2023-01-07 12:54:17 +0500

    Worked with GitHub

---
+++ Installed GitHub.cpp
@@ -0,0 +1,76 @@
+Programming Fundamental
+Programming Day - Week 01
+
+
+LINK
+
+
+https://github.com/
+
+
+PRACTICED FOLLOWING COMMANDS
+
+
+
+git config --global user.email “email”
+
+Its a one time use only command, that is used to connect the
+GitBash with your GitHub Account
+
+
+
+git config --global user.name "name"
+
+This is a one time use only command that is used to connect the
+user remotely with their GitHub Account.
+
+
+
+git config --global github.user “myusername”
+
+This is a one time use only command that is used to verify your
+GitHub account user name.
+
+
+
+git config --global github.token “mytoken”
+
+This is a one time use only command that is used to give access to
+the GitBash so it can access your account.
+
+
+
+git init This is used to iniliazte the session.
+
+It is used every time you want to create a new repo on the GitHub
+account.
+
+
+
+git remote add origin https://… 
+
+This command associates all the committed files to the defied
+origin repo.
+
+
+
+git add . 
+
+This command is used to add all the files in the current working
+directory to the list of files that are to be uploaded on the GitHub
+account.
+
+
+
+git commit -m “m”
+
+This command makes the commit along with the message that you
+want to associate with that commit.
+
+
+
+
+git push -u origin master
+
+This command is used to upload all the committed files to the
+defined origin repo.
